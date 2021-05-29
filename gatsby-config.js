d9fbb56f7509e16db08bf0e32eaa080206eba989
Tucker Bickler <tbickler@expediagroup.com>
2021-05-29 00:32:44 -0600

    adding styles

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: "gatsby-plugin-manifest",
       options: {
-        icon: "src/images/icon.png",
+        icon: "src/images/dogecoin-300.png",
       },
     },
     "gatsby-plugin-mdx",
